1ccc5c9cceca06bc7cfdf1ed87e8cf618c6b0153
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-22 22:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const high=document.getElementsByTagName("STRONG");
+	for(let i=0;i<high.length;i++){
+		high[i].style.color="green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+    const high=document.getElementsByTagName("STRONG");
+	for(let i=0;i<high.length;i++){
+		high[i].style.color="black";
+	}
     
 }
